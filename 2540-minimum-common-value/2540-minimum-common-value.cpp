22d535c1d1c730bee13929621d0133391132c18c
ARITRA MANDAL <83087717+ARITRA2004@users.noreply.github.com>
2026-05-19 20:01:40 +0530

    Time: 0 ms (100%), Space: 54.5 MB (58.81%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        int right = 0;
+        int left = 0;
+
+        while(left < nums1.size() && right < nums2.size()){
+            if(nums1[left] < nums2[right]) left++;
+            else if(nums1[left] > nums2[right]) right++;
+            else return nums1[left];
+        }
+        return -1;
+    }
+};
