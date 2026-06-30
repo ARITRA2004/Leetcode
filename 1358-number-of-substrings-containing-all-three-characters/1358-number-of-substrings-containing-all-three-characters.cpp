5d4d2f1dabcd190dce4ce26080ab60b8d4974cd0
ARITRA MANDAL <83087717+ARITRA2004@users.noreply.github.com>
2026-06-30 12:53:02 +0530

    Time: 7 ms (65.28%), Space: 11 MB (81.8%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int last_seen[3] = {-1,-1,-1};
+
+        int n = s.size();
+
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            last_seen[s[i] - 'a'] = i;
+
+            if(last_seen[0] != -1 && last_seen[1] != -1 && last_seen[2] != -1){
+                count += 1 + min({last_seen[0],last_seen[1],last_seen[2]});
+            }
+        }
+        return count;
+    }
+};
