0cd5b2d863f5ae8d0fee74bf95e66f76868db766
ARITRA MANDAL <83087717+ARITRA2004@users.noreply.github.com>
2026-08-04 11:12:36 +0530

    Time: 777 ms (5.07%), Space: 91.3 MB (50.51%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+
+        int n = capacity.size();
+
+        vector<int>diff(n);
+
+        for(int i=0;i<n;i++){
+            diff[i] = capacity[i] - rocks[i];
+        }
+
+        sort(diff.begin(),diff.end());
+
+        // for(int ele:diff) cout << ele << " ";
+        // cout << endl;
+
+        for(int i=0;i<n;i++){
+            if(diff[i] == 0) continue;
+            if(diff[i] < additionalRocks && additionalRocks > 0){
+                int rem = additionalRocks - diff[i];
+                additionalRocks = rem;
+                diff[i] = 0;
+            }else if(diff[i] == additionalRocks){
+                diff[i] = 0;
+                break;
+            }
+            else{
+                break;
+            }
+        }
+
+        int count = 0;
+
+        for(int ele:diff) cout << ele << " ";
+        cout << endl;
+
+        for(int ele:diff){
+            if(ele == 0) count++;
+        }
+        return count;
+    }
+};
